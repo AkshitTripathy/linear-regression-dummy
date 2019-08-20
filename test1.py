ef40e01cc60c3af4b874c03f83b221f0ce6b8ba2
Mayank Anand <mayankan@gmail.com>
2019-08-20 19:15:11 +0530

    Changes in Code

--- test1.py
+++ test1.py
@@ -2,9 +2,9 @@ import numpy as np #imports numpy
 from sklearn.linear_model import LinearRegression #imports Linear Regression Model from skikit learn
 x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1)) #Creates an array of 6 elements, reshapes it to y axis and saves it in X
 y = np.array([5, 20, 14, 32, 22, 38]) #Creates an array of 6 elements using numpy
-model = LinearRegression().fit(x,y) #Fits LinearRegression Model to X and Y
+model = LinearRegression().fit(x,y) #Fits X and Y to LinearRegression Model
 r_sq = model.score(x, y) #Calculates R2(square) of Linear Regression model
-A = float(input("Enter the value of x")) #takes input from user for value of x to predict value of y
+A = float(input("Enter the value of x = ")) #takes input from user for value of x to predict value of y
 A = np.array([A]).reshape((-1, 1)) #Creates array of value taken from the user
 B = model.predict(A) #Predicts Value of y using model and value taken from user
 #model.intercept_ + model.coef_ * A 
--- test2.py
+++ test2.py
@@ -1,15 +1,15 @@
-import numpy as np
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-from pandas.plotting import scatter_matrix
-url = "data/test_data.csv"
-dataset = pd.read_csv(url, header=0)
-X = dataset['X'].to_numpy()
-Y = dataset['Y'].to_numpy()
-model = LinearRegression().fit(X.reshape((-1, 1)),Y)
-A_input = int(input("Enter the value of x = "))
-A = np.array([A_input]).reshape((-1, 1))
-B = model.predict(A) #model.intercept_ + model.coef_ * A
-print('Predicted Value is ',B)
-input_dataset = pd.DataFrame({'X': A_input, 'Y': B})
-input_dataset.to_csv(url, mode='a', header=False, index=False)
+import numpy as np #imports numpy and sets an alias as np
+import pandas as pd #imports pandas and sets an alias as pd
+from sklearn.linear_model import LinearRegression #imports Linear Regression Model from skikit learn
+url = "data/test_data.csv" #Sets URL of the dataset from and to which is taken for setting and updating model
+dataset = pd.read_csv(url, header=0) #uses pandas read_csv method to fetch dataset from url and sets header 1st line as names
+X = dataset['X'].to_numpy() #gets X column from dataset, converts it to numpy array using to_numpy() and saves it to X
+Y = dataset['Y'].to_numpy() #gets Y column from dataset, converts it to numpy array using to_numpy() and saves it to Y
+model = LinearRegression().fit(X.reshape((-1, 1)),Y) #Fits X and Y to LinearRegression Model
+A_input = float(input("Enter the value of x = ")) #takes input from user for value of x to predict value of y
+A = np.array([A_input]).reshape((-1, 1)) #Creates array of value taken from the user
+B = model.predict(A) #Predicts Value of y using model and value taken from user
+#model.intercept_ + model.coef_ * A
+print('Predicted Value is ',B) #prints predicted value
+input_dataset = pd.DataFrame({'X': A_input, 'Y': B}) #Creates a pandas dataframe of x value from user input and y value from predicted value and stores it into input_dataset
+input_dataset.to_csv(url, mode='a', header=False, index=False) #appends input_dataset to same dataset from url using to_csv function and removes header and index
